dee48805a5dd263721185f7f9471b4937eb08da5
Tauhee-d <thohidbaig2000@gmail.com>
2023-01-05 16:23:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client'
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+  const socket = io.connect("http://localhost:5000")
+  
+
+    socket.on('table',(event) => {
+        const newData = JSON.parse(event.data);
+          setData((prevData) => [...prevData, newData]);
+
+    })
+
+    // socket.onmessage = (event) => {
+    //   const newData = JSON.parse(event.data);
+    //   setData((prevData) => [...prevData, newData]);
+    // };
+
+    return () => {
+      socket.close();
+      console.log("data",data)
+    };
+  }, []);
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h1>hii</h1>
+       <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Battery</th>
+          <th>Temperature</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row) => (
+          <tr key={row.id}>
+            <td>{row.col1}</td>
+            <td>{row.col2}</td>
+            <td>{row.col3}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+
+   
+
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
